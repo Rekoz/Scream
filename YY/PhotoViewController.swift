794e4422d8ad0d841c085bfaec25e8b803f4e231
REKOZ <jiangyntz@gmail.com>
2014-11-08 16:05:43 -0800

    photo v

--- YY/PhotoViewController.swift
+++ YY/PhotoViewController.swift
@@ -1,28 +1,23 @@
-//
-//  PhotoViewController.swift
-//  YY
-//
-//  Created by Qikun Guo on 11/8/14.
-//
-//
-
-import Foundation
 import UIKit
+import AVFoundation
 
 class PhotoViewController:UIViewController {
 
-    @IBOutlet weak var img1: UIImageView!
-    var photo:Photo = Photo()
+    @IBOutlet weak var img: UIImageView!
+    var photo = Photo()
+    
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         //fatalError("init(coder:) has not been implemented")
     }
     
-    override init() {
-        super.init()
+    
+    @IBAction func buttonSpeakClicked(sender: UIButton) {
+        var mySpeechSynthesizer:AVSpeechSynthesizer = AVSpeechSynthesizer()
+        var myString:String = "This is the phrase to say"
+        var mySpeechUtterance:AVSpeechUtterance = AVSpeechUtterance(string:myString)
+        mySpeechSynthesizer.speakUtterance(mySpeechUtterance)
     }
     
-    
 }
-
