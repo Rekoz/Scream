d7b958334e9447559bf9edc8727e3d838eba51e7
REKOZ <jiangyntz@gmail.com>
2014-11-08 16:41:28 -0800

    partial fix

--- YY/AppDelegate.swift
+++ YY/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        var yynvc = YYNavViewController()
+        let frame = UIScreen.mainScreen().bounds
+        window = UIWindow(frame: frame)
+        
+        window!.rootViewController = yynvc
+        window!.makeKeyAndVisible()
         return true
     }
 
